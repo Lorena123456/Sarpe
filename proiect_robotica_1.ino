d677e3dbbe573bfd1d174ba573a5c8c186131a6b
Lorena123456 <46710724+Lorena123456@users.noreply.github.com>
2019-01-20 00:14:26 +0200

    Update proiect_robotica_1.ino

--- proiect_robotica_1.ino
+++ proiect_robotica_1.ino
@@ -1,91 +1,78 @@
-#include <LedControl.h> // Biblioteca LedControl este utilizată pentru controlul unei matrice LED.
-#include <LiquidCrystal.h> //Biblioteca LiquidCrystal este utilizată pentru controlul LCD.
+#include <LedControl.h>
+#include <LiquidCrystal.h>
 
-LiquidCrystal lcd (2, 3, 4, 5, 6, 7);
 LedControl lc = LedControl (12, 11, 10, 1);
+LiquidCrystal lcd (2, 3, 4, 5, 6, 7);
 
 //definim pinii
 #define JOYSTICK_X  A0
 #define JOYSTICK_Y  A1
+#define sus       1
+#define dreapta   2
+#define jos       3
+#define stanga    4
+#define lungimeinitialasarpe  3
+#define joystickprag  160
 
-const short lungimeinitialasarpe = 3; //lungimea inițială a șarpelui (1-63, recomandată 3)
-
-// parametrii sarpelui
+int sarpeDirectie = 0;
+int sarpeViteza = 1;
 int sarpeLungime = lungimeinitialasarpe;
-int sarpeViteza = 1; // nu poate fi 0
-int sarpeDirectie = 0; // dacă este 0, șarpele nu se mișcă
-
-// directia constantelor
-const short sus     = 1;
-const short dreapta  = 2;
-const short jos   = 3;
-const short stanga   = 4;
-
 bool castigator = false;
 bool pierdere = false;
-
-const int joystickprag = 160; // prag în cazul în care mișcarea joystick-ului va fi acceptată
-
-struct Point {
-  int row = 0;
-  int col = 0;
-  Point(int row = 0, int col = 0): row(row), col(col) {}
-};
-
-struct Coordinate {
+struct Coordonate {
   int x = 0;
   int y = 0;
-  Coordinate(int x = 0, int y = 0): x(x), y(y) {}
+  Coordonate(int x = 0, int y = 0): x(x), y(y) {}
+};
+struct Punct {
+  int linie = 0;
+  int coloana = 0;
+  Punct(int linie = 0, int coloana = 0): linie(linie), coloana(coloana) {}
 };
 
-Point sarpe; // coordonatele de la care pornește șarpele vor fi generate aleator
-Point mancare (-1, -1); // punctul de hrană nu este nicăieri încă
-Coordinate initialJoystick (500, 500);  //construiți cu valorile implicite în cazul în care utilizatorul oprește calibrarea
-Coordinate values;
+Punct sarpe; // coordonatele de la care pornește șarpele vor fi generate aleator
+Punct mancare (-1, -1); // punctul de hrană nu este nicăieri încă
+Coordonate initialJoystick (500, 500);
+Coordonate valuare;
 
-int matrice[8][8]; // matricea "matrice": deține o "vârstă" a fiecărui pixel din matrice. Dacă numărul> 0, acesta luminează.
-// când vârsta unui anumit pixel depășește lungimea șarpelui, acesta se stinge.
-// 1 se adaugă în direcția actuală de șarpe de lângă ultima poziție a capului de șarpe.
+int matrice[8][8];
 
 void setup() {
-  Serial.begin(9600); // setează rata de date în biți pe secundă pentru transmiterea de date seriale.
-  lc.shutdown(0, false); // opriți economisirea energiei, permite afișarea
+  Serial.begin(9600);
+  lc.shutdown(0, false);
   lc.setIntensity(0, 9);
-  lc.clearDisplay(0); // clear screen
+  lc.clearDisplay(0);
 
   randomSeed(analogRead(0));
-  sarpe.row = random(8);
-  sarpe.col = random(8);
-
-  calibrateJoystick();
+  sarpe.linie = random(8);
+  sarpe.coloana = random(8);
 
   lcd.begin(16, 2);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("SNAKE GAME<Start");
+  lcd.print("   Press start");
   lcd.setCursor(0, 1);
-  lcd.print("  Move Joystick");
 }
 
 void loop() {
-  genereazamancarea(); // dacă nu există alimente, generați unul
-  joystickMiscare(); // priveste miscarile joystick-ului
-  sarpeMiscare(); // calculează parametrii de șarpelui
-  gameStates();
+  genereazaMancarea();
+  joystickMiscare();
+  sarpeMiscare();
+  stadiileJocului();
 }
 
 //dacă nu există hrană, generați una, verificați și pentru victorie
-void genereazamancarea() {
-  if (mancare.row == -1 || mancare.col == -1) {
+void genereazaMancarea() {
+  if (mancare.linie == -1 || mancare.coloana == -1) {
     if (sarpeLungime >= 64) {
       castigator = true;
-      return; //împiedicați funcționarea generatorului de alimente, în acest caz se va desfășura pentru totdeauna
+      return; //împiedicați funcționarea generatorului de mancare, în acest caz se va desfășura pentru totdeauna
     }
     // genereaza mancarea
     do {
-      mancare.col = random(8);
-      mancare.row = random(8);
-    } while (matrice[mancare.row][mancare.col] > 0);
+      mancare.coloana = random(8);
+      mancare.linie = random(8);
+    } while (matrice[mancare.linie][mancare.coloana] > 0);
   }
 }
 
@@ -105,120 +92,109 @@ void joystickMiscare() {
     analogRead(JOYSTICK_X) < initialJoystick.x - joystickprag ? sarpeDirectie = stanga  : 0;
     analogRead(JOYSTICK_X) > initialJoystick.x + joystickprag ? sarpeDirectie = dreapta : 0;
 
-    // ignorați schimbarea direcțională cu 180 de grade (fără efect pentru șarpele care nu se mișcă)
-    sarpeDirectie + 2 == anterioaraDirectie && anterioaraDirectie != 0 ? sarpeDirectie = anterioaraDirectie : 0;
-    sarpeDirectie - 2 == anterioaraDirectie && anterioaraDirectie != 0 ? sarpeDirectie = anterioaraDirectie : 0;
-
     //clipirea manacarii
-    lc.setLed(0, mancare.row, mancare.col, millis() % 100 < 50 ? 1 : 0);
+    lc.setLed(0, mancare.linie, mancare.coloana, millis() % 100 < 50 ? 1 : 0);
   }
 }
 
-// calculeaza datele despre mișcarea șarpelui
+
 void sarpeMiscare() {
   switch (sarpeDirectie) {
     case sus:
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Try not to lose!");
-      sarpe.row--;
-      edge();
-      lc.setLed(0, sarpe.row, sarpe.col, 1);
+      lcd.print("Have fun!");
+      sarpe.linie--;
+      margine();
+      lc.setLed(0, sarpe.linie, sarpe.coloana, 1);
       break;
 
     case dreapta:
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Try not to lose!");
-      sarpe.col++;
-      edge();
-      lc.setLed(0, sarpe.row, sarpe.col, 1);
+      lcd.print("Have fun!");
+      sarpe.coloana++;
+      margine();
+      lc.setLed(0, sarpe.linie, sarpe.coloana, 1);
       break;
 
     case jos:
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Try not to lose!");
-      sarpe.row++;
-      edge();
-      lc.setLed(0, sarpe.row, sarpe.col, 1);
+      lcd.print("Have fun!");
+      sarpe.linie++;
+      margine();
+      lc.setLed(0, sarpe.linie, sarpe.coloana, 1);
       break;
 
     case stanga:
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Try not to lose!");
-      sarpe.col--;
-      edge();
-      lc.setLed(0, sarpe.row, sarpe.col, 1);
+      lcd.print("Have fun!");
+      sarpe.coloana--;
+      margine();
+      lc.setLed(0, sarpe.linie, sarpe.coloana, 1);
       break;
 
     default:
       return;
   }
 
-  if (matrice[sarpe.row][sarpe.col] != 0 && sarpeDirectie != 0) {
+  if (matrice[sarpe.linie][sarpe.coloana] != 0 && sarpeDirectie != 0) {
     pierdere = true;
     return;
   }
 
   // verificați dacă mâncarea a fost consumată
-  if (sarpe.row == mancare.row && sarpe.col == mancare.col) {
+  if (sarpe.linie == mancare.linie && sarpe.coloana == mancare.coloana) {
     sarpeLungime++;
-    mancare.row = -1; // reseteaza mancarea
-    mancare.col = -1;
+    mancare.linie = -1; // reseteaza mancarea
+    mancare.coloana = -1;
   }
 
-  // vârstele de creștere dacă toate led-urile aprinse
-  updateMatrice();
+  Matrice();
 
-  // schimbați vârsta capului de șarpe de la 0 la 1
-  matrice[sarpe.row][sarpe.col]++;
+  matrice[sarpe.linie][sarpe.coloana]++;
 }
 
-// determină ca șarpele să apară pe cealaltă parte a ecranului dacă se iese din margine
-void edge() {
-
-  if (sarpe.col < 0) {
-    sarpe.col = 7;
+void margine() {
+  if (sarpe.coloana < 0) {
+    sarpe.coloana = 7;
   }
-  else if (sarpe.col > 7) {
-    sarpe.col = 0;
+  else if (sarpe.coloana > 7) {
+    sarpe.coloana = 0;
   }
 
-  if (sarpe.row  < 0) {
-    sarpe.row = 7;
+  if (sarpe.linie  < 0) {
+    sarpe.linie = 7;
   }
-  else if (sarpe.row > 7) {
-    sarpe.row = 0;
+  else if (sarpe.linie > 7) {
+    sarpe.linie = 0;
   }
 }
 
-
-// vârstele de creștere dacă toate led-urile aprinse, opriți la cele vechi în funcție de lungimea șarpelui
-void updateMatrice() {
-  for (int row = 0; row < 8; row++) {
-    for (int col = 0; col < 8; col++) {
-      if (matrice[row][col] > 0 ) {
-        matrice[row][col]++;
+void Matrice() {
+  for (int linie = 0; linie < 8; linie++) {
+    for (int coloana = 0; coloana < 8; coloana++) {
+      if (matrice[linie][coloana] > 0 ) {
+        matrice[linie][coloana]++;
       }
 
-      //dacă vârsta depășește lungimea șarpei, opriți-o
-      if (matrice[row][col] > sarpeLungime) {
-        lc.setLed(0, row, col, 0);
-        matrice[row][col] = 0;
+      if (matrice[linie][coloana] > sarpeLungime) {
+        lc.setLed(0, linie, coloana, 0);
+        matrice[linie][coloana] = 0;
       }
     }
   }
 }
 
-void gameStates() {
+void stadiileJocului() {
   if (pierdere || castigator) {
-    unrollSnake();
-    scoreMessage(sarpeLungime);
+    revenireSarpe();
+    mesajScor(sarpeLungime);
     if (pierdere) {
       lcd.setCursor(2, 1);
-      lcd.print("Start Again!");
+      lcd.print("Press Start!");
     }
     castigator = false;
     pierdere = false;
@@ -229,15 +205,14 @@ void gameStates() {
   }
 }
 
-void unrollSnake() {
-  // opriți LED-ul pentru mancare
-  lc.setLed(0, mancare.row, mancare.col, 0);
+void revenireSarpe() {
+  lc.setLed(0, mancare.linie, mancare.coloana, 0);
   delay(600);
   for (int i = 1; i <= sarpeLungime; i++) {
-    for (int row = 0; row < 8; row++) {
-      for (int col = 0; col < 8; col++) {
-        if (matrice[row][col] == i) {
-          lc.setLed(0, row, col, 0);
+    for (int linie = 0; linie < 8; linie++) {
+      for (int coloana = 0; coloana < 8; coloana++) {
+        if (matrice[linie][coloana] == i) {
+          lc.setLed(0, linie, coloana, 0);
           delay(100);
         }
       }
@@ -245,23 +220,11 @@ void unrollSnake() {
   }
 }
 
-// calibrați joystick-ul de 10 ori
-void calibrateJoystick() {
-  for (int i = 0; i < 10; i++) {
-    values.x += analogRead(JOYSTICK_X);
-    values.y += analogRead(JOYSTICK_Y);
-  }
-
-  initialJoystick.x = values.x / 10;
-  initialJoystick.y = values.y / 10;
-}
-
-void scoreMessage(int score) {
-  if (score < 0 || score > 99) return;
-
+void mesajScor(int scor) {
+  if (scor < 0 || scor > 99) return;
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("GameOverScore:");
+  lcd.print("      Score:");
   lcd.setCursor(14, 0);
-  lcd.print(score);
+  lcd.print(scor);
 }
